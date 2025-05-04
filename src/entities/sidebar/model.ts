5d7d4a236977fb63a8e3057c08a21ddc7e1d81f1
Кирилл Донцов <KDontsov@integrity-solutions.ru>
2025-05-05 00:52:30 +0300

    initial commit

---
+++ src/entities/sidebar/model.ts
@@ -0,0 +1,15 @@
+import { defineStore } from "pinia";
+import { useLocalStorage } from '@vueuse/core'
+
+export const useSidebarStore = defineStore('sidebar', {
+  state: () => ({ expanded: useLocalStorage('sidebar-expanded', 'true') }),
+  getters: {},
+  actions: {
+    setExpanded(value: boolean) {
+      this.expanded = value;
+    },
+    toggleExpanded() {
+      this.expanded = !this.expanded;
+    },
+  },
+})
---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  rollupOptions: {
+    input: "src/main.js",
+    format: "system",
+    preserveEntrySignatures: true,
+  },
+  base: "http://localhost:9003",
+  plugins: [
+    vue({
+      template: {
+        transformAssetUrls: {
+          base: "/src",
+        },
+      },
+    }),
+    tailwindcss(),
+  ],
+})
